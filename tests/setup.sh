fc474a740b530f4462b6bd201e8111a0a59f7b4c
siegfried <zhiqiang.lei@gmail.com>
2020-03-15 22:31:02 +0800

    Support OpenBSD (#11)

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
 OS=`uname`
+USER=`whoami`
 
 case $OS in
     "Linux")
-        USER=`whoami`
         set -x
         sudo ip tuntap add dev tun10 mode tun user $USER
         sudo ip address add 10.10.10.1/24 dev tun10
         sudo ip link set tun10 up
+        ;;
+    "OpenBSD")
+        set -x
+        doas ifconfig tun10 create
+        doas ifconfig tun10 inet 10.10.10.1 10.10.10.2 netmask 255.255.255.255
+        cd /dev
+        doas sh MAKEDEV tun10
+        doas chown $USER:$USER tun10
+        cd -
         ;;
     *)
         printf "%s is not supported.\n" $OS >&2
