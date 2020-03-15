fa31894cde806380777598583a00d7178a9ac41b
siegfried <zhiqiang.lei@gmail.com>
2020-03-15 14:19:34 +0800

    Exit when OS is not supported (#13)

--- tests/run.sh
+++ tests/run.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-set -x
-set -e
+OS=`uname`
 
-case `uname` in
+case $OS in
     "Linux")
+        set -x
         cargo build --verbose
         cargo test --verbose
         cargo build --verbose --target x86_64-unknown-linux-musl
         cargo test --verbose --target x86_64-unknown-linux-musl
         ;;
-    "*") ;;
+    *)
+        set -x
+        cargo build --verbose
+        cargo test --verbose
+        ;;
 esac
--- tests/setup.sh
+++ tests/setup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-set -x
+OS=`uname`
 
-case `uname` in
+case $OS in
     "Linux")
-        sudo ip tuntap add dev tun10 mode tun user `whoami`
+        USER=`whoami`
+        set -x
+        sudo ip tuntap add dev tun10 mode tun user $USER
         sudo ip address add 10.10.10.1/24 dev tun10
         sudo ip link set tun10 up
         ;;
-    "*") ;;
+    *)
+        printf "%s is not supported.\n" $OS >&2
+        exit 1
+        ;;
 esac
