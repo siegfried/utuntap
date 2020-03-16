c284fa731c9ed6572b191a28feb852395ed88737
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2020-03-16 17:44:59 +0800

    Fix clean.sh

--- tests/clean.sh
+++ tests/clean.sh
@@ -6,11 +6,13 @@ case $OS in
     "Linux")
         set -x
         sudo ip tuntap del tun10 mode tun
-        sudo ip tuntap del tap10 mode tap
+        sudo ip tuntap del tun11 mode tun
+        sudo ip tuntap del tap11 mode tap
         ;;
     "OpenBSD")
         doas ifconfig tun10 destroy
-        doas ifconfig tap10 destroy
+        doas ifconfig tun11 destroy
+        doas ifconfig tap11 destroy
         ;;
     *)
         printf "%s is not supported.\n" $OS >&2
