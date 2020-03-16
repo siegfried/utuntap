33b14f95359426d0be6cb14278d14a3009f8d376
siegfried <zhiqiang.lei@gmail.com>
2020-03-16 17:46:53 +0800

    Implement nonblock option (#17)

--- tests/clean.sh
+++ tests/clean.sh
@@ -6,9 +6,13 @@ case $OS in
     "Linux")
         set -x
         sudo ip tuntap del tun10 mode tun
+        sudo ip tuntap del tun11 mode tun
+        sudo ip tuntap del tap11 mode tap
         ;;
     "OpenBSD")
         doas ifconfig tun10 destroy
+        doas ifconfig tun11 destroy
+        doas ifconfig tap11 destroy
         ;;
     *)
         printf "%s is not supported.\n" $OS >&2
--- tests/setup.sh
+++ tests/setup.sh
@@ -9,14 +9,24 @@ case $OS in
         sudo ip tuntap add dev tun10 mode tun user $USER
         sudo ip address add 10.10.10.1/24 dev tun10
         sudo ip link set tun10 up
+        sudo ip tuntap add dev tun11 mode tun user $USER
+        sudo ip link set tun11 up
+        sudo ip tuntap add dev tap11 mode tap user $USER
+        sudo ip link set tap11 up
         ;;
     "OpenBSD")
         set -x
         doas ifconfig tun10 create
         doas ifconfig tun10 inet 10.10.10.1 10.10.10.2 netmask 255.255.255.255
+        doas ifconfig tun11 create
+        doas ifconfig tap11 create
         cd /dev
         doas sh MAKEDEV tun10
         doas chown $USER:$USER tun10
+        doas sh MAKEDEV tun11
+        doas chown $USER:$USER tun11
+        doas sh MAKEDEV tap11
+        doas chown $USER:$USER tap11
         cd -
         ;;
     *)
