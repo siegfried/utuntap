e7875fb0cf83a40afb47592c990e28e50135c95a
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-12-26 23:28:24 +0800

    Improve document

--- src/tun.rs
+++ src/tun.rs
@@ -50,133 +50,145 @@ pub struct OpenOptions {
 }
 
 impl OpenOptions {
-    /// Creates a blank new set of options ready for configuration.
-    ///
-    /// All options are initially set to `false` except read and write.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.open(0).unwrap();
-    /// ```
+    /**
+    Creates a blank new set of options ready for configuration.
+
+    All options are initially set to `false` except read and write.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.open(0).unwrap();
+    ```
+    */
     pub fn new() -> Self {
         let mut options = super::OpenOptions::new();
         options.mode(Mode::Tun);
         Self { options }
     }
 
-    /// Sets the option for read access.
-    ///
-    /// This option, when true, will indicate that the file should be
-    /// `read`-able if opened.
-    ///
-    /// This opiton defaults to `true`.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.read(true).write(true).open(0).unwrap();
-    /// ```
+    /**
+    Sets the option for read access.
+
+    This option, when true, will indicate that the file should be
+    `read`-able if opened.
+
+    This opiton defaults to `true`.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.read(true).write(true).open(0).unwrap();
+    ```
+    */
     pub fn read(&mut self, value: bool) -> &mut Self {
         self.options.read(value);
         self
     }
 
-    /// Sets the option for write access.
-    ///
-    /// This option, when true, will indicate that the file should be
-    /// `write`-able if opened.
-    ///
-    /// This opiton defaults to `true`.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.read(true).write(true).open(0).unwrap();
-    /// ```
+    /**
+    Sets the option for write access.
+
+    This option, when true, will indicate that the file should be
+    `write`-able if opened.
+
+    This opiton defaults to `true`.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.read(true).write(true).open(0).unwrap();
+    ```
+    */
     pub fn write(&mut self, value: bool) -> &mut Self {
         self.options.write(value);
         self
     }
 
-    /// Sets the option for non-blocking I/O.
-    ///
-    /// This option, when true, will indicate that the file should not
-    /// block for data to become available.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.nonblock(true).open(0).unwrap();
-    /// ```
+    /**
+    Sets the option for non-blocking I/O.
+
+    This option, when true, will indicate that the file should not
+    block for data to become available.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.nonblock(true).open(0).unwrap();
+    ```
+    */
     #[cfg(target_family = "unix")]
     pub fn nonblock(&mut self, value: bool) -> &mut Self {
         self.options.nonblock(value);
         self
     }
 
-    /// Sets the option for packet info.
-    ///
-    /// This option, when true, will indicate that each packet read or
-    /// written is prefixed with a 4-byte packet info.
-    ///
-    /// This option is only available on Linux.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.packet_info(true).open(0).unwrap();
-    /// ```
+    /**
+    Sets the option for packet info.
+
+    This option, when true, will indicate that each packet read or
+    written is prefixed with a 4-byte packet info.
+
+    This option is only available on Linux.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.packet_info(true).open(0).unwrap();
+    ```
+    */
     #[cfg(target_os = "linux")]
     pub fn packet_info(&mut self, value: bool) -> &mut Self {
         self.options.packet_info(value);
         self
     }
 
-    /// Opens a tun device file with the options specified by `self`.
-    ///
-    /// # Arguments
-    ///
-    /// * `number` - the number of the device, e.g. the "0" of "tun0".
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error under a number of different
-    /// circumstances. Some of these error conditions are listed here, together
-    /// with their [`ErrorKind`]. The mapping to [`ErrorKind`]s is not part of
-    /// the compatibility contract of the function.
-    ///
-    /// * [`NotFound`]: The device file does not exist.
-    /// * [`PermissionDenied`]: The user lacks permission to get the specified
-    ///   access rights for the file.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use utuntap::tun::OpenOptions;
-    ///
-    /// let mut options = OpenOptions::new();
-    /// let file = options.open(0).unwrap();
-    /// ```
-    ///
-    /// [`ErrorKind`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html
-    /// [`NotFound`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html#variant.NotFound
-    /// [`PermissionDenied`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html#variant.PermissionDenied
+    /**
+    Opens a tun device file with the options specified by `self`.
+
+    # Arguments
+
+    * `number` - the number of the device, e.g. the "0" of "tun0".
+
+    # Errors
+
+    This function will return an error under a number of different
+    circumstances. Some of these error conditions are listed here, together
+    with their [`ErrorKind`]. The mapping to [`ErrorKind`]s is not part of
+    the compatibility contract of the function.
+
+    * [`NotFound`]: The device file does not exist.
+    * [`PermissionDenied`]: The user lacks permission to get the specified
+      access rights for the file.
+
+    # Examples
+
+    ```no_run
+    use utuntap::tun::OpenOptions;
+
+    let mut options = OpenOptions::new();
+    let file = options.open(0).unwrap();
+    ```
+
+    [`ErrorKind`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html
+    [`NotFound`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html#variant.NotFound
+    [`PermissionDenied`]: https://doc.rust-lang.org/nightly/std/io/enum.ErrorKind.html#variant.PermissionDenied
+    */
     pub fn open(&mut self, number: u32) -> Result<File> {
         Ok(self.options.open(number)?)
     }
